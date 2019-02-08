d4751fe001628893b379c383450e590480e2f4eb
Fowzulhafsa29 <47441268+Fowzulhafsa29@users.noreply.github.com>
2019-02-08 14:40:22 +0530

    Create sampledemo4.py

---
+++ sampledemo4.py
@@ -0,0 +1,10 @@
+z=str(raw_input())
+if(z.isalpha()):
+    if(z=="a" or z=="e" or z=="i" or z=="o" or z=="u"):
+      print("Vowel")
+    else:
+      print("Consonant")
+else:
+    print("invalid")
+  
+  
